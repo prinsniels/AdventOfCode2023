68d13858794ca9426d1c3f39c07207a29cb61470
Niels Prins <niels.prins@gmail.com>
2023-12-07 09:01:48 +0100

    minor improvement

--- src/main/scala/solutions/day07.scala
+++ src/main/scala/solutions/day07.scala
@@ -15,6 +15,9 @@ def strenght(inp: String) =
     case _                => 7
 
 def upgraded(inp: String): String =
+  /**
+    * Upgrade the hand by changing the J to the most common character
+    */
   val g  = inp.groupBy(identity).mapValues(s => s.length()).toMap
   val js = g.getOrElse('J', 0)
   if js == 0 || js == 5 then inp
@@ -28,46 +31,44 @@ object Card:
       case s"$h $s" => Card(h, s.toLong)
 
 def one(inp: List[String]) =
-  lazy val worth = "AKQJT987654321".zipWithIndex.toMap
+  val worth = "AKQJT987654321".zipWithIndex.toMap
 
   def compare(l: Card, r: Card): Boolean =
     val leftStrength = strenght(l.h)
     val rightStength = strenght(r.h)
 
-    if (leftStrength != rightStength) leftStrength < rightStength
+    if (leftStrength != rightStength) leftStrength > rightStength
     else
       l.h.toList.zip(r.h.toList).find((lc, rc) => lc != rc) match
         case None           => false
-        case Some((lc, rc)) => worth(lc) < worth(rc)
+        case Some((lc, rc)) => worth(lc) > worth(rc)
 
   inp
-    .map(Card.parse)
-    .sortWith(compare)
-    .reverse
-    .zipWithIndex
-    .map((c, s) => c.bid * (s + 1))
-    .sum pipe println
+    .map(Card.parse)                  // parse
+    .sortWith(compare)                // sort with the comparing function
+    .zipWithIndex                     // add a rank
+    .map((c, s) => c.bid * (s + 1))   // calculate the score
+    .sum pipe println                 // sum and output
 
 def two(inp: List[String]) =
-  lazy val worth = "AKQT987654321J".zipWithIndex.toMap
+  val worth = "AKQT987654321J".zipWithIndex.toMap
 
   def compare(l: Card, r: Card): Boolean =
     val leftStrength = strenght(upgraded(l.h))
     val rightStength = strenght(upgraded(r.h))
 
-    if (leftStrength != rightStength) leftStrength < rightStength
+    if (leftStrength != rightStength) leftStrength > rightStength
     else
       l.h.toList.zip(r.h.toList).find((lc, rc) => lc != rc) match
         case None           => false
-        case Some((lc, rc)) => worth(lc) < worth(rc)
+        case Some((lc, rc)) => worth(lc) > worth(rc)
 
   inp
-    .map(Card.parse)
-    .sortWith(compare)
-    .reverse
-    .zipWithIndex
-    .map((c, s) => c.bid * (s + 1))
-    .sum pipe println
+    .map(Card.parse)                    // parse
+    .sortWith(compare)                  // sort with the comparing function
+    .zipWithIndex                       // add a rank
+    .map((c, s) => c.bid * (s + 1))     // calculate the score
+    .sum pipe println                   // sum and output
 
 object Solution extends App:
   one("day07.txt".live)
